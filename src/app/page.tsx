cb3407dfd61f40e326fa7565a8ec241b37a966ce
J2 DevStudio <j2devstudio@gmail.com>
2025-10-25 21:37:49 -0700

    features design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import { Header } from "@/sections/Header";
 import { Hero } from "@/sections/Hero";
+import { LogoTicker } from "@/sections/LogoTicker";
+import { Features } from "@/sections/Features";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Hero />
+      <LogoTicker />
+      <Features />
     </>
   );
 }
