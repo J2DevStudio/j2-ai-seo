62cb219dcecc9772d31018b3f1422958b3cba75a
J2 DevStudio <j2devstudio@gmail.com>
2025-10-26 17:47:30 -0700

    footer design

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,58 @@
+import LogoIcon from "@/assets/logo.svg";
+import XSocial from "@/assets/social-x.svg";
+import InstaSocial from "@/assets/social-instagram.svg";
+import YTSocial from "@/assets/social-youtube.svg";
+
 export const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="py-5 border-t border-white/15">
+      <div className="container">
+        <div className="flex flex-col lg:flex-row items-center gap-8">
+          <div className="flex gap-2 items-center lg:flex-1">
+            <LogoIcon className="h-6 w-6" />
+            <div className="font-medium">AI SEO &nbsp;|&nbsp; J2 DevStudio</div>
+          </div>
+
+          <nav className="flex flex-col lg:flex-row gap-5 lg:gap-7 lg:flex-1 lg:justify-center text-center">
+            <a
+              href="#"
+              className="text-sm text-white/70 hover:text-white transition"
+            >
+              Features
+            </a>
+            <a
+              href="#"
+              className="text-sm text-white/70 hover:text-white transition"
+            >
+              Developers
+            </a>
+            <a
+              href="#"
+              className="text-sm text-white/70 hover:text-white transition"
+            >
+              Company
+            </a>
+            <a
+              href="#"
+              className="text-sm text-white/70 hover:text-white transition"
+            >
+              Blog
+            </a>
+            <a
+              href="#"
+              className="text-sm text-white/70 hover:text-white transition"
+            >
+              Changelog
+            </a>
+          </nav>
+
+          <div className="flex gap-7 lg:flex-1 lg:justify-end">
+            <XSocial className="text-white/40 hover:text-white transition" />
+            <InstaSocial className="text-white/40 hover:text-white transition" />
+            <YTSocial className="text-white/40 hover:text-white transition" />
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
